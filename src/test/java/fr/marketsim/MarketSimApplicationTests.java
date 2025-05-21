7b7262779dd467f5192cf9145872087ebcb514fe
Anthony <anthony.rousseau26@gmail.com>
2025-05-21 09:42:25 +0200

    Initial Commit

---
+++ src/test/java/fr/marketsim/MarketSimApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.marketsim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketSimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
